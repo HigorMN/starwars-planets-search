0332386128f589200db715f31eca3ef797274c7b
HigorMN <higor.maranhao2000@gmail.com>
2022-10-19 15:35:38 -0300

    renomenando meu context

---
+++ src/context/ProviderStarwars.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import ContextStarwars from './ContextStarwars';
+
+function ProviderStarWars({ children }) {
+  const [dataApiPlanets, setdataApiPlanets] = useState([]);
+  useEffect(() => {
+    const fetchApiPlanets = async () => {
+      const endpoint = 'https://swapi.dev/api/planets';
+      const { results } = await (await fetch(endpoint)).json();
+      setdataApiPlanets(results);
+    };
+    fetchApiPlanets();
+  }, []);
+  return (
+    <ContextStarwars.Provider value={ dataApiPlanets }>
+      {children}
+    </ContextStarwars.Provider>
+  );
+}
+
+ProviderStarWars.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProviderStarWars;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Provider from './context/myProvider';
+import ProviderStarWars from './context/ProviderStarwars';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <Provider>
+    <ProviderStarWars>
       <App />
-    </Provider>,
+    </ProviderStarWars>,
   );
