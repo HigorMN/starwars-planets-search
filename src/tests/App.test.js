70cd1cfc2ee00916a3967ad6aca48976c4771ce5
HigorMN <higor.maranhao2000@gmail.com>
2022-10-21 18:36:09 -0300

    Requisito 10 concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,22 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
+const ASC = [  
+  'Yavin IV', 'Tatooine',
+  'Bespin',   'Endor',
+  'Kamino',   'Alderaan',
+  'Naboo',    'Coruscant',
+  'Hoth',     'Dagobah'
+];
+
+const DESC = [ 
+  'Coruscant', 'Naboo',
+  'Alderaan',  'Kamino',
+  'Endor',     'Bespin',
+  'Tatooine',  'Yavin IV',
+  'Hoth',      'Dagobah'
+];
+
 describe('Testando a aplicação', () => {
   test('Se todos os filtros aparece na tela', async () => {
     render(<App />);
@@ -69,7 +85,6 @@ describe('Testando a aplicação', () => {
 
   test('Testando o igual a', () => {
     render(<App />);
-    const columnFilter = screen.getByTestId('column-filter');
     const comparisonFilter = screen.getByTestId('comparison-filter');
     const valueFilter = screen.getByTestId('value-filter');
     const buttonFilter = screen.getByTestId('button-filter');
@@ -90,15 +105,82 @@ describe('Testando a aplicação', () => {
     const buttonFilter = screen.getByTestId('button-filter');
 
     const tatooineName = await screen.findByText(/tatooine/i, {}, {timeout: 20000})
-    
     expect(tatooineName).toBeInTheDocument();
 
+    userEvent.selectOptions(columnFilter, 'orbital_period');
+    userEvent.selectOptions(comparisonFilter, 'maior que');
+    userEvent.clear(valueFilter);
+    userEvent.type(valueFilter, '20');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'diameter');
+    userEvent.selectOptions(comparisonFilter, 'menor que');
+    userEvent.type(valueFilter, '20000');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'population');
+    userEvent.selectOptions(comparisonFilter, 'igual a');
+    userEvent.type(valueFilter, '2000000000');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'rotation_period');
+    userEvent.selectOptions(comparisonFilter, 'igual a');
+    userEvent.type(valueFilter, '24');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'surface_water');
+    userEvent.selectOptions(comparisonFilter, 'igual a');
+    userEvent.type(valueFilter, '40');
+    userEvent.click(buttonFilter);
+
+    expect(screen.getByText(/Alderaan/i)).toBeInTheDocument();
+
+    expect(comparisonFilter.value).toBe('igual a');
+    expect(columnFilter.value).toBe('');
+
+    const allBtnRemove = screen.getAllByText(/excluir/i);
+
+    allBtnRemove.forEach((e) => {
+      userEvent.click(e);
+    })
+    expect(tatooineName).toBeInTheDocument();
+  });
+
+  jest.setTimeout(40000)
+  test('Adicionando e removendo todos os filtros e tensando o maior que', async () => {
+    render(<App />);
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const buttonFilter = screen.getByTestId('button-filter');
+
+    const tatooineName = await screen.findByText(/tatooine/i, {}, {timeout: 20000})
+    expect(tatooineName).toBeInTheDocument();
+
+    userEvent.selectOptions(columnFilter, 'orbital_period');
+    userEvent.selectOptions(comparisonFilter, 'maior que');
+    userEvent.clear(valueFilter);
+    userEvent.type(valueFilter, '20');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'diameter');
+    userEvent.selectOptions(comparisonFilter, 'menor que');
+    userEvent.type(valueFilter, '20000');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'population');
+    userEvent.selectOptions(comparisonFilter, 'igual a');
+    userEvent.type(valueFilter, '2000000000');
+    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'rotation_period');
     userEvent.selectOptions(comparisonFilter, 'maior que');
     userEvent.type(valueFilter, '20');
     userEvent.click(buttonFilter);
-    userEvent.click(buttonFilter);
-    userEvent.click(buttonFilter);
-    userEvent.click(buttonFilter);
+    userEvent.clear(valueFilter);
+    userEvent.selectOptions(columnFilter, 'surface_water');
+    userEvent.selectOptions(comparisonFilter, 'maior que');
+    userEvent.type(valueFilter, '20');
     userEvent.click(buttonFilter);
 
     expect(screen.getByText(/Alderaan/i)).toBeInTheDocument();
@@ -111,7 +193,37 @@ describe('Testando a aplicação', () => {
     allBtnRemove.forEach((e) => {
       userEvent.click(e);
     })
+    expect(tatooineName).toBeInTheDocument();
+  });
 
-    expect(screen.getByText(/tatooine/i)).toBeInTheDocument();
+  jest.setTimeout(40000)
+  test('Testando se ordenda de forma Ascendente e Desendente a população', async () => {
+    render(<App />);
+    const columnSort = screen.getByTestId('column-sort');
+    const inputAsc = screen.getByTestId('column-sort-input-asc');
+    const InputDesc = screen.getByTestId('column-sort-input-desc');
+    const buttonOrder = screen.getByTestId('column-sort-button');
+
+    expect(columnSort).toBeInTheDocument();
+    expect(inputAsc).toBeInTheDocument();
+    expect(InputDesc).toBeInTheDocument();
+    expect(buttonOrder).toBeInTheDocument();
+
+    const tatooineName = await screen.findByText(/tatooine/i, {}, {timeout: 20000})
+    expect(tatooineName).toBeInTheDocument();
+
+    userEvent.selectOptions(columnSort, 'population');
+    userEvent.click(inputAsc);
+    userEvent.click(buttonOrder);
+
+    const planetNames = screen.getAllByTestId('planet-name');
+    planetNames.forEach((names, index) => {
+      expect(names.innerHTML).toBe(ASC[index]);
+    })
+    userEvent.click(InputDesc);
+    userEvent.click(buttonOrder);
+    planetNames.forEach((names, index) => {
+      expect(names.innerHTML).toBe(DESC[index]);
+    })
   });
 })
